bb8fcedb6f1e22db7cd2432b33b6d6ccfcfb4124
The Infosec Guy <keshavaarav22@gmail.com>
2021-02-28 14:15:38 +0530

    Modularized Functions & Added Node Arg & Functionality

--- bin/create_server.js
+++ bin/create_server.js
@@ -9,33 +9,28 @@ const mkdirp = require('mkdirp');
 const cwd = process.cwd();
 const fs = require('fs-extra');
 const fs_helper = require('../lib/build_helper');
+const file_creator = require('../lib/file_generator');
 
 program
   .version(pkg.version)
   .option('-f, --framework <type>', 'Enter Name of Framework')
   .requiredOption('-n, --name <type>', 'Enter Name of App')
-  .option('--mvc <type>', 'Enter 0 if MVC Dir Structure is not needed')
+  .option('--mvc', 'Enter 0 if MVC Dir Structure is not needed')
   .parse(process.argv)
 
 
 const options = program.opts();
 const appName = options.name;
 const folderDir = path.join(cwd, appName);
-const templatesDir = path.join(__dirname, "..",'templates', "express");
+const templatesDirExpress = path.join(__dirname, "..",'templates', "express");
+const templatesDirNode = path.join(__dirname, "..",'templates', "node");
 
 // Create Directory with the App Name
 mkdirp.sync(folderDir);
 
 // Create Folders for MVC if MVC Flag is set
-if(options.mvc != 0){
-    fs_helper.createDir(folderDir, "views");
-    fs_helper.createDir(folderDir, "model");
-    fs_helper.createDir(folderDir, "controllers");
-}
+(options.mvc) ? file_creator.generateMVC(folderDir) : null;
 
-function createIndexJS(){
-  let basePath = path.join(templatesDir, "index.js");
-  fs_helper.buildFilewithContents(basePath, folderDir, "index.js");
-}
+options.framework == 'express' ? file_creator.createExpressApp(templatesDirExpress, folderDir) : file_creator.createNodeApp(templatesDirExpress, folderDir)
 
-createIndexJS();
+file_creator.createExpressApp(templatesDirExpress, folderDir);
