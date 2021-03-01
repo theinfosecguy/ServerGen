53f0bb086576f42e3b212ac919f81745dfbd7275
The Infosec Guy <keshavaarav22@gmail.com>
2021-03-01 19:49:52 +0530

    Views Arg Functionality Added + Package.json for Node/Express

--- bin/create_server.js
+++ bin/create_server.js
@@ -2,33 +2,35 @@
 
 console.log("Project on branch by Keshav started !");
 
-const program = require('commander');
-const pkg = require('../package.json');
-const path = require('path');
-const mkdirp = require('mkdirp');
+const program = require("commander");
+const pkg = require("../package.json");
+const path = require("path");
+const mkdirp = require("mkdirp");
 const cwd = process.cwd();
-const fs = require('fs-extra');
-const fs_helper = require('../lib/build_helper');
-const file_creator = require('../lib/file_generator');
+const fs = require("fs-extra");
+const fs_helper = require("../lib/build_helper");
+const file_creator = require("../lib/file_generator");
 
 program
   .version(pkg.version)
-  .option('-f, --framework <type>', 'Enter Name of Framework')
-  .requiredOption('-n, --name <type>', 'Enter Name of App')
-  .option('--views <type>', 'Which View Engine to use!')
-  .parse(process.argv)
-
+  .option("-f, --framework <type>", "Enter Name of Framework")
+  .requiredOption("-n, --name <type>", "Enter Name of App")
+  .option("-v --view <type>", "Which View Engine to use!")
+  .parse(process.argv);
 
 const options = program.opts();
 const appName = options.name;
 const folderDir = path.join(cwd, appName);
-const templatesDirExpress = path.join(__dirname, "..",'templates', "express");
-const templatesDirNode = path.join(__dirname, "..",'templates', "node");
+const templatesDirExpress = path.join(__dirname, "..", "templates", "express");
+const templatesDirNode = path.join(__dirname, "..", "templates", "node");
+const viewsDir = path.join(__dirname, "..", "templates", "express", "views");
 
 // Create Directory with the App Name
 mkdirp.sync(folderDir);
 
-// Create views for MVC if MVC Flag is set
-//Todo - Views setup
+options.framework == "node"
+  ? file_creator.createNodeApp(templatesDirNode, folderDir, appName)
+  : file_creator.createExpressApp(templatesDirExpress, folderDir, appName);
 
-options.framework == 'node'? file_creator.createNodeApp(templatesDirNode, folderDir) : file_creator.createExpressApp(templatesDirExpress, folderDir)
+// Handle View Flag
+file_creator.handleViews(folderDir, viewsDir, options.view);
