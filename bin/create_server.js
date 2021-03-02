347f8729c1b528ce11005c9c622b281cc7aaa194
The Infosec Guy <keshavaarav22@gmail.com>
2021-03-02 21:03:07 +0530

    Version 1.0.0 Completed

--- bin/create_server.js
+++ bin/create_server.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const chalk = require("chalk");
 const program = require("commander");
 const pkg = require("../package.json");
 const path = require("path");
@@ -8,12 +9,13 @@ const cwd = process.cwd();
 const fs = require("fs-extra");
 const fs_helper = require("../lib/build_helper");
 const file_creator = require("../lib/file_generator");
+const displayer = require("../lib/log_display");
 
 program
   .version(pkg.version)
-  .option("-f, --framework <type>", "Enter Name of Framework")
+  .option("-f, --framework <type>", "Enter Name of Framework: Node | Express")
   .requiredOption("-n, --name <type>", "Enter Name of App")
-  .option("-v --view <type>", "Which View Engine to use!")
+  .option("-v --view <type>", "Name of View Engine: Pug | Jade | EJS | HBS")
   .option("--config ", "Install Mongoose & the Folder Directory for it")
   .parse(process.argv);
 
@@ -25,7 +27,7 @@ const templatesDirNode = path.join(__dirname, "..", "templates", "node");
 const viewsDir = path.join(__dirname, "..", "templates", "express", "views");
 
 // Create Directory with the App Name
-mkdirp.sync(folderDir);
+fs_helper.buildFolderforApp(folderDir);
 
 options.framework == "node"
   ? file_creator.createNodeApp(templatesDirNode, folderDir, appName)
@@ -41,3 +43,4 @@ options.Config
   : null;
 
 file_creator.addGitIgnore(folderDir, templatesDirExpress);
+displayer.endMessage(appName);
