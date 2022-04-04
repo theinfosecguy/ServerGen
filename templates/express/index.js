943e3ef225c0e6fc5ed38690afdcf824f827dcb6
theinfosecguy <keshavaarav22@gmail.com>
2022-04-04 16:31:14 +0530

    Set URL Encoded Extended to True

--- templates/express/index.js
+++ templates/express/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/", require("./routes"));
