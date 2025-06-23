4bb6a6e9effa3c24b4009eddf2693877febec91e
Keshav Malik <33570148+theinfosecguy@users.noreply.github.com>
2025-06-23 21:44:51 +0530

    style: add formatter config and clean files

--- templates/express/config/mongoose.js
+++ templates/express/config/mongoose.js
@@ -1,15 +1,18 @@
-const mongoose = require("mongoose"); // Required Library
+const mongoose = require('mongoose'); // Required Library
 
-mongoose.connect("mongodb://localhost/demo_db", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/demo_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Acquire the connection - To access DB
 const db = mongoose.connection;
 
 // To have a check on Error
-db.on("error", console.error.bind(console, "connection error:"));
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // Once Connection is Open
-db.once("open", function () {
+db.once('open', function () {
   // we're connected!
-  console.log("Connected to DB");
+  console.log('Connected to DB');
 });
--- templates/express/routes/index.js
+++ templates/express/routes/index.js
@@ -3,17 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.json({
-        message: 'Welcome to ServerGen!'
-    }).status(200);
+  res
+    .json({
+      message: 'Welcome to ServerGen!',
+    })
+    .status(200);
 });
 
 router.post('/', function (req, res) {
-    res.json({
-        message: 'Welcome to ServerGen!',
-        data: req.body
-    }).status(200);
+  res
+    .json({
+      message: 'Welcome to ServerGen!',
+      data: req.body,
+    })
+    .status(200);
 });
-
 
 module.exports = router;
